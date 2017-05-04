5cf1e7c4c8a500a3759bccb97547e5e5fbd0cfc3
rikke59 <sveull16@student.westerdals.no>
2017-05-04 12:13:29 +0200

    Php

---
+++ menubar.php
@@ -0,0 +1,91 @@
+
+<style>
+#container_main{
+  position: absolute;
+    top: 0%;
+    left: 0%;
+    width:100%;
+    overflow: hidden;
+    height: 100%
+
+}
+
+#menubar_container {
+  position: absolute;
+    top: 0%;
+    left: 0%;
+    width:100%;
+    height: 170px;
+    margin: 0;
+
+
+}
+
+</style>
+<div id="container_main">
+<div id="menubar_container">
+
+  <a href="#Barer">
+  <div id="Bar_menu_element_bg">
+    <h3 id="Menu_text">BARER</h3>
+    <div id="Bar_menu_element_line">
+    </div>
+  </div>
+  <div id="Hjem_menu_logo_bar_box">
+    <img id="Hjem_menu_logo_wht" src="./Bilder/logo_menubar/w_logo_wht.png"/>
+  <img id="Hjem_menu_logo_bar" src="./Bilder/logo_menubar/w_logo_prpl.png"/>
+  </div>
+</a>
+
+ <a href="#Restaurant">
+  <div id="Restaurant_menu_element_bg">
+    <h3 id="Menu_text">RESTAURANTER</h3>
+    <div id="Restaurant_menu_element_line">
+    </div>
+  </div>
+  <div id="Hjem_menu_logo_rest_box">
+    <img id="Hjem_menu_logo_wht" src="./Bilder/logo_menubar/w_logo_wht.png"/>
+  <img id="Hjem_menu_logo_rest" src="./Bilder/logo_menubar/w_logo_ble.png"/>
+  </div>
+</a>
+
+  <a href="#Hjem">
+  <div id="Hjem_menu_element_bg"></div>
+  <div id="Hjem_menu_element_extend">
+    <div id="Hjem_Trapes_Farge"></div>
+    <div id="Hjem_Trapes_Hvit"></div>
+    <div id="Hjem_Linje_Hoyre"></div>
+    <div id="Hjem_Linje_Venstre"></div>
+  </div>
+  <div id="Hjem_menu_element_border"></div>
+  <div id="Hjem_menu_logo_box">
+    <img id="Hjem_menu_logo" src="./Bilder/logo_menubar/w_logo.png"/>
+  </div>
+</a>
+
+  <a href="#Studere">
+  <div id="Studere_menu_element_bg">
+    <h3 id="Menu_text">STUDERE</h3>
+    <div id="Studere_menu_element_line">
+    </div>
+  </div>
+  <div id="Hjem_menu_logo_stud_box">
+    <img id="Hjem_menu_logo_wht" src="./Bilder/logo_menubar/w_logo_wht.png"/>
+  <img id="Hjem_menu_logo_stud" src="./Bilder/logo_menubar/w_logo_ylw.png"/>
+  </div>
+</a>
+
+<div id="Sok_menu_element_bg">
+  <div id="search-box-wrapper">
+      <input type="text" placeholder="Hvor vil du?" id="search-box-input">
+      <input id="search-box-button" type="image" src="./Bilder/icons/search.png" />
+  </div>
+  <div id="Sok_menu_element_line"></div>
+</div>
+<div id="Hjem_menu_logo_sok_box">
+  <img id="Hjem_menu_logo_wht" src="./Bilder/logo_menubar/w_logo_wht.png"/>
+<img id="Hjem_menu_logo_sok" src="./Bilder/logo_menubar/w_logo_grn.png"/>
+</div>
+
+</div>
+</div>
