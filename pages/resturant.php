bd95153b3423e0ef461ff3947257c4c8cb1427eb
rikke59 <sveull16@student.westerdals.no>
2017-05-04 13:04:37 +0200

    Endring av linker og sletting av unødvendig kode

--- pages/resturant.php
+++ pages/resturant.php
@@ -3,13 +3,13 @@
 <head>
 
     <meta charset="utf-8">
-    <link rel="stylesheet" href="./css/menubar.css">
-    <link rel="stylesheet" href="./css/filter_menu.css">
+    <link rel="stylesheet" href="../css/menubar.css">
+    <link rel="stylesheet" href="../css/filter_menu.css">
     <title>Resturanter</title>
 </head>
 <body>
 
-      <?php require 'menubar.php' ?>
+      <?php require '../assets/menubar.php' ?>
 
 
 
