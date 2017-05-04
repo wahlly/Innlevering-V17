b74526b3dc11e4677e3fd38f8ba4c3cf4dde1e44
wahmar <wahmar16@student.westerdals.no>
2017-05-04 12:41:12 +0200

    Borte

--- MartinTest/Test.php
+++ MartinTest/Test.php
@@ -13,39 +13,7 @@ table, th, td {
 </head>
 <body>
 
-  <?php
-  $servername = "martinwahlberg.no.mysql";
-  $username = "martinwahlberg_no_westerdals_";
-  $password = "westerdals123";
-  $dbname = "martinwahlberg_no_westerdals_";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT * FROM sted";
-$result = $conn->query($sql);
-$css_pososv = array();
-$i = 0;
-if ($result->num_rows > 0) {
-     // output data of each row
-     while($row = $result->fetch_assoc()) {
-         $css_pososv[$i] = $row["Navn"];
-         $i++;
-     }
-} else {
-     echo "0 results";
-}
-$conn->close();
-$arrlength = count($css_pososv);
-for($x = 0; $x < $arrlength; $x++) {
-    echo $css_pososv[$x];
-    echo "<br>";
-}
-?>
+  
 
 
 </body>
