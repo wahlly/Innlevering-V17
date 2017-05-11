4b1306a93abe5ff889cefbd938ffb99b6ba3925c
rikke59 <sveull16@student.westerdals.no>
2017-05-11 13:06:08 +0200

    legger til inforside

---
+++ pages/cafesarah.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nb">
+<head>
+
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/menubar.css">
+    <link rel="stylesheet" href="../css/filter_menu.css">
+    <title>Cafe Sarah</title>
+</head>
+<body>
+
+      <?php require '../assets/menubar.php' ?>
+
+
+
+</body>
+</html>
