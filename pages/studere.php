eef0e5fb122af1e8326578810c71de76488f227a
rikke59 <sveull16@student.westerdals.no>
2017-05-11 19:51:25 +0200

    Starta med Studie/rest/bar sider

--- pages/studere.php
+++ pages/studere.php
@@ -6,10 +6,58 @@
     <link rel="stylesheet" href="../css/menubar.css">
     <link rel="stylesheet" href="../css/filter_menu.css">
     <title>Studie</title>
+
+    <style>
+      #container_m {
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+
+      #card_container {
+        background-color: #f2f2f2;
+        position: absolute;
+        margin-left: 10%;
+        top: 30%;
+        width: 80%;
+        height: 100%;
+      }
+
+      #footer {
+        position: absolute;
+        width: 100%;
+        height: 20%;
+        top: 140%;
+        border-top: solid grey 1px;
+
+      }
+
+      .card{
+        position: absolute;
+        margin: auto;
+        width: 20%;
+        height: 35%;
+        background-color: blue;
+      }
+    </style>
 </head>
 <body>
+      <div id="container_m">
+        <?php require '../assets/menubar.php' ?>
 
-      <?php require '../assets/menubar.php' ?>
+        <div id="card_container">
+          <div class="card" id="card1"></div>
+          <div class="card" id="card2"></div>
+          <div class="card" id="card3"></div>
+          <div class="card" id="card4"></div>
+          </div>
+
+        </div>
+
+        <div id="footer"></div>
+
+      </div>
 
 
 
