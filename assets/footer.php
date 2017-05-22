ec43b6ff2421062534b8dacc1663fe3c65f5c642
wahmar <wahmar16@student.westerdals.no>
2017-05-22 14:51:43 +0200

    Footer?

---
+++ assets/footer.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+  <style>
+  #footer_container{
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25%;
+  width: 100%;
+  background-color: orange;
+  }
+  #footerbox1{
+    position: absolute;
+    left: 0%;
+    height: 100%;
+    width: 20%;
+    background-color: #6b3e72;
+    color: white;
+    text-align: center;
+    font-family: sans-serif;
+  }
+  #footerbox2{
+    position: absolute;
+    left: 20%;
+    height: 100%;
+    width: 20%;
+    background-color: #38a3bc;
+    color: white;
+    text-align: center;
+    font-family: sans-serif;
+  }
+  #footerbox3{
+    position: absolute;
+    left: 40%;
+    height: 100%;
+    width: 20%;
+    background-color: #ff504a;
+    color: white;
+    text-align: center;
+    font-family: sans-serif;
+  }
+  #footerbox4{
+    position: absolute;
+    left: 60%;
+    height: 100%;
+    width: 20%;
+    background-color: #ffca41;
+    color: white;
+    text-align: center;
+    font-family: sans-serif;
+  }
+  #footerbox5{
+    position: absolute;
+    left: 80%;
+    height: 100%;
+    width: 20%;
+    background-color: #52b97d;
+    color: white;
+    text-align: center;
+    font-family: sans-serif;
+  }
+  </style>
+</head>
+<body>
+<div id="footer_container">
+<div id="footerbox1"><h3> ÅPNINGSTIDER FJERDINGEN</h3> <h4>Mandag - Fredag: 07:30-24:00</br>Lørdag: 10:00-23:00</br>Søndag: 10:00-23:00</h4></div>
+<div id="footerbox2"><h3> ÅPNINGSTIDER VULKAN</h3> <h4>Mandag - Fredag: 07:30-24:00</br>Lørdag: 10:00-23:00</br>Søndag: 10:00-23:00</h4></div>
+<div id="footerbox3"><h3> ÅPNINGSTIDER BRENNERIVEIEN</h3> <h4>Mandag - Fredag: 07:30-24:00</br>Lørdag: 10:00-23:00</br>Søndag: 10:00-23:00</h4></div>
+<div id="footerbox4"><a href="/pages/kontaktoss.php" style="z-index:1; text-decoration:none; color:white;"><h3>Leter du etter mer nyttig informasjon?</br> KLIKK HER</h3></a></div>
+<div id="footerbox5"><a href="/pages/kontaktoss.php#oss" style="z-index:1; text-decoration:none; color:white;"><h3>Lurer du på hvem vi er?</br> KLIKK HER</h3></a></div>
+</div>
+</body>
+</html>
