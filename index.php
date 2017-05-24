b6e395414aa69485c6173abe57fa97611db424b7
Tobben00 <tobias.nikolai.mars@gmail.com>
2017-05-24 19:08:21 +0200

    Riktig Ico
    Sweet

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html>
 
 <head>
-  <link rel="shortcut icon" type="image/png" href="/img/img_layout/layout_menubar/w_logo.png"/>
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="/favicon.ico" type="image/x-icon">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <link rel="stylesheet" href="./css/menubar.css">
   <link rel="stylesheet" href="./css/filter_menu.css">
